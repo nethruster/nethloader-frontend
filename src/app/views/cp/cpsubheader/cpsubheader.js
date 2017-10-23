125db75e1afef84e3390f66abfca80bc90839ef7
gariasf <gariasfa7@alumnes.ub.edu>
2017-10-23 20:27:00 +0200

    Add new icons to cp opverview

--- src/app/views/cp/cpsubheader/cpsubheader.js
+++ src/app/views/cp/cpsubheader/cpsubheader.js
@@ -1,6 +1,8 @@
 import { h, Component } from 'preact'
 import { NavLink } from 'react-router-dom'
 
+import Icon from '../../shared/icon/icon.js'
+
 import style from './cpsubheader.scss'
 
 import locale from 'locale'
@@ -13,14 +15,14 @@ export default class CPSubheader extends Component {
       <div class={`${style.cpsubheader} flex flex-main-center`}>
         <div class={`${style.cpsubheaderWrapper} flex flex-cross-center flex-sb`}>
           <div class={style.cpsubheaderUserStats}>
-            <p>Name</p>
+            <p>Claudio</p>
             <p>X {viewStrings.uploads}</p>
           </div>
           <div class={`${style.cpsubheaderTabs} flex flex-cross-center`}>
             <nav class='flex flex-cross-center flex-sa'>
-              <NavLink exact to='/cp/' class='flex flex-full-center'>{viewStrings.tabs.overview}</NavLink>
-              <NavLink exact to='/cp/settings' class='flex flex-full-center'>{viewStrings.tabs.settings}</NavLink>
-              <NavLink exact to='/cp/neth-admin' class='flex flex-full-center'>{viewStrings.tabs.admin_settings}</NavLink>
+              <NavLink exact to='/cp/'><Icon iconName='overview' />&nbsp;{viewStrings.tabs.overview}</NavLink>
+              <NavLink exact to='/cp/settings'><Icon iconName='settings' />&nbsp;{viewStrings.tabs.settings}</NavLink>
+              <NavLink exact to='/cp/neth-admin' data-adminbutton><Icon iconName='admin-settings' />&nbsp;{viewStrings.tabs.admin_settings}</NavLink>
             </nav>
           </div>
         </div>
