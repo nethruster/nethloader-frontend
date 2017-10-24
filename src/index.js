154e419ca539fd42fc2089a5fbb5f8ede213be70
gariasf <gariasfa7@alumnes.ub.edu>
2017-10-24 19:49:31 +0200

    Fix babel module issue

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import { h, render } from 'preact'
 
 import asyncComponent from 'asyncComponent'
 
-if (module.hot) {
-  require('preact/debug')
-}
-
 let App = asyncComponent(() => import(/* webpackChunkName: "app" */'./app/app.js').then(module => module.default))
 
 const mountPoint = document.getElementById('nethloader')
