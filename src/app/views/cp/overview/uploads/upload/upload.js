11f1e32f9db06a540120fdfe4fa787001f79e81b
gariasf <gariasfa7@alumnes.ub.edu>
2017-10-29 13:22:35 +0100

    Add copy url to clipboard functionality & add dynamic date format calculation

--- src/app/views/cp/overview/uploads/upload/upload.js
+++ src/app/views/cp/overview/uploads/upload/upload.js
@@ -16,6 +16,7 @@ export default class Upload extends Component {
   computeDate () {
     let date = new Date(this.props.upDate)
 
+    // Note: We add 1 to the month because months are counted from 0
     return `${pad(date.getDate())}-${pad(date.getMonth() + 1)}-${date.getFullYear()}`
   }
 
@@ -25,14 +26,20 @@ export default class Upload extends Component {
     return `${pad(date.getHours())}:${pad(date.getMinutes())}`
   }
 
+  computeFormat () {
+    let date = new Date(this.props.upDate)
+
+    return `DD/MM/YYY GMT${date.getTimezoneOffset() / 60}`
+  }
+
   render () {
     const testImage = 'https://images.unsplash.com/photo-1484199408980-5918a796a53f?dpr=1&auto=format&fit=crop&w=1373&q=60&cs=tinysrgb'
     return (
       <li class={`${style.upload} flex flex-cross-center flex-sb`} data-id={this.props.id}>
-        <div class={`${style.uploadImage} flex flex-full-center`}><Link target='_blank' rel='noopener' to={`/${this.props.id}`}><AsyncMedia src={testImage} /></Link></div>
+        <div class={`${style.uploadImage} flex flex-full-center`} title='Click to open'><Link target='_blank' rel='noopener' to={`/${this.props.id}`}><AsyncMedia src={testImage} /></Link></div>
         <div class={`${style.uploadDataSpec} flex flex-dc`}><p><span>ID:</span> {this.props.id}</p> <p><span>{viewStrings.type}:</span> {this.props.type}</p></div>
-        <div class={`${style.uploadDataDate} flex flex-dc`} title='DD-MM-YYYY - GMT+02'><p><span>{viewStrings.uploaded}:</span> {this.computeDate()}</p> <p><span>{viewStrings.at}:</span> {this.computeTime()}</p></div>
-        <div class={`${style.uploadButtons} flex flex-dc flex-sa`}><a href={testImage} download><Button small text={viewStrings.download} icon='download' customClass={style.uploadButtonsButton} /></a><a><Button small icon='copy' text={viewStrings.copy_url} customClass={style.uploadButtonsButton} /></a></div>
+        <div class={`${style.uploadDataDate} flex flex-dc`} title={this.computeFormat()}><small>{this.computeFormat()}</small><p><span>{viewStrings.uploaded}:</span> {this.computeDate()}</p> <p><span>{viewStrings.at}:</span> {this.computeTime()}</p></div>
+        <div class={`${style.uploadButtons} flex flex-dc flex-sa`}><a href={testImage} download><Button small text={viewStrings.download} icon='download' customClass={style.uploadButtonsButton} /></a><a><Button small icon='copy' text={viewStrings.copy_url} copyText={`${window.location.origin}/${this.props.id}`} customClass={style.uploadButtonsButton} /></a></div>
       </li>
     )
   }
--- src/app/views/shared/button/button.js
+++ src/app/views/shared/button/button.js
@@ -1,27 +1,76 @@
 import { h, Component } from 'preact'
+import {CopyToClipboard} from 'react-copy-to-clipboard'
 
 import style from './button.scss'
 import Icon from '../icon/icon.js'
 import Spinner from '../spinner/spinner.js'
 
 export default class Button extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      copy: {
+        valueCopied: false
+      }
+    }
+
+    this.handleValueCopied = this.handleValueCopied.bind(this)
+  }
+
+  handleValueCopied () {
+    let copy = {
+      ...this.state.copy
+    }
+
+    copy.valueCopied = true
+    this.setState({copy})
+
+    setTimeout(() => {
+      copy.valueCopied = false
+      this.setState({copy})
+    }, 2000)
+  }
+
   render () {
-    return (
-      <button
-        class={`${style.button}
-              ${this.props.round ? style.buttonRound : ''}
-              ${this.props.contrast ? style.buttonContrast : ''}
-              ${this.props.big ? style.buttonBig : ''}
-              ${this.props.small ? style.buttonSmall : ''}
-              ${this.props.transparent ? style.buttonTransparent : ''}
-              ${this.props.navButton ? style.buttonNav : ''}
-              ${this.props.customClass ? this.props.customClass : ''}
-              flex flex-full-center` }
-        tabindex={this.props.tabindex}
-        onClick={this.props.onClickExecute}>
-        {this.props.icon ? <Icon iconName={this.props.icon} /> : null}&nbsp;
-        <p class='flex'>{this.props.spinner ? <Spinner color={this.props.spinnerColor} size={this.props.spinnerSize} /> : this.props.text}&nbsp;</p>
-      </button>
-    )
+    if (this.props.copyText) {
+      return (
+        <CopyToClipboard text={this.props.copyText} onCopy={this.handleValueCopied}>
+          <button
+            class={`${style.button}
+                  ${this.props.round ? style.buttonRound : ''}
+                  ${this.props.contrast ? style.buttonContrast : ''}
+                  ${this.props.big ? style.buttonBig : ''}
+                  ${this.props.small ? style.buttonSmall : ''}
+                  ${this.props.transparent ? style.buttonTransparent : ''}
+                  ${this.props.navButton ? style.buttonNav : ''}
+                  ${this.props.customClass ? this.props.customClass : ''}
+                  flex flex-full-center` }
+            tabindex={this.props.tabindex}
+            onClick={this.props.onClickExecute}>
+            {this.props.icon ? <Icon iconName={this.props.icon} /> : null}&nbsp;
+            <p class='flex'>{this.props.spinner ? <Spinner color={this.props.spinnerColor} size={this.props.spinnerSize} /> : this.state.copy.valueCopied ? 'Copied!' : this.props.text}&nbsp;</p>
+          </button>
+        </CopyToClipboard>
+      )
+    } else {
+      return (
+        <button
+          class={`${style.button}
+                ${this.props.round ? style.buttonRound : ''}
+                ${this.props.contrast ? style.buttonContrast : ''}
+                ${this.props.big ? style.buttonBig : ''}
+                ${this.props.small ? style.buttonSmall : ''}
+                ${this.props.transparent ? style.buttonTransparent : ''}
+                ${this.props.navButton ? style.buttonNav : ''}
+                ${this.props.customClass ? this.props.customClass : ''}
+                flex flex-full-center` }
+          tabindex={this.props.tabindex}
+          onClick={this.props.onClickExecute}>
+          {this.props.icon ? <Icon iconName={this.props.icon} /> : null}&nbsp;
+          <p class='flex'>{this.props.spinner ? <Spinner color={this.props.spinnerColor} size={this.props.spinnerSize} /> : this.props.text}&nbsp;</p>
+        </button>
+      )
+    }
   }
 }
