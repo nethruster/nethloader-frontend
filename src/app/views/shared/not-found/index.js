3d19fccb61d14bfe19ad4f0d6639be063d2334b0
gariasf <gariasfa7@alumnes.ub.edu>
2017-11-19 22:43:19 +0100

    Add not found (404) page

--- src/app/views/shared/not-found/index.js
+++ src/app/views/shared/not-found/index.js
@@ -1,10 +1,25 @@
 import { h, Component } from 'preact'
+import { Link } from 'react-router-dom'
+
+import { adminEmail } from 'app.config'
+
+import style from './styles.scss'
 
 export default class NotFound extends Component {
   render () {
     return (
-      <div class='flex flex-full-center'>
-        <p>Not found :(</p>
+      <div class={`flex flex-full-center flex-dc ${style.notFound}`}>
+        <p class='nomedia flex flex-full-center flex-dc'>
+          <picture>
+            <source src='./assets/img/sorry.webp' />
+            <source src='./assets/img/sorry.gif' />
+            <img src='./assets/img/sorry.gif' />
+          </picture>
+          <span class='flex flex-full-center'>Sorry, we couldn't find that...</span>
+          <small>
+            If something should be here, <a href={`mailto:${adminEmail}`}>contact with an administrator</a> or <Link to='/cp/overview'>go back to the control panel.</Link>
+          </small>
+        </p>
       </div>
     )
   }
--- src/assets/app.config.js
+++ src/assets/app.config.js
@@ -4,8 +4,11 @@ const apiBaseUrl = 'http://localhost:4000/graphql'
 // IMPORTANT: Has to preserve the last trailing slash
 const baseMediaPath = 'http://localhost:4000/media/'
 
+const adminEmail = 'supportadmin@domain.com'
+
 export {
   apiBaseUrl,
   version,
-  baseMediaPath
+  baseMediaPath,
+  adminEmail
 }
