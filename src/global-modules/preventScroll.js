49d975c0765ae6c8dfe5fdf8e3923cef59d7a104
gariasf <gariasfa7@alumnes.ub.edu>
2017-11-23 14:36:36 +0100

    Fix broken radio input class

--- src/global-modules/preventScroll.js
+++ src/global-modules/preventScroll.js
@@ -1,9 +1,12 @@
+/**
+ * This script provides an interface to block and unblock document scrolling, including keypresses and touch events.
+ */
+
 /* List of keys that can cause scrolling
  *
  * spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
  * left: 37, up: 38, right: 39, down: 40,
  */
-
 const scrollKeys = [32, 33, 34, 35, 36, 37, 38, 39, 40]
 let pageXOffset,
   pageYOffset
@@ -13,32 +16,32 @@ export function scrollBlockOn () {
   pageXOffset = window.pageXOffset
   pageYOffset = window.pageYOffset
 
-  document.addEventListener('scroll', scrollHandler)
-  document.addEventListener('wheel', defaultHandler)
-  document.addEventListener('DOMMouseScroll', defaultHandler)
-  document.addEventListener('touchmove', defaultHandler)
-  document.addEventListener('keydown', keydownHandler)
+  document.addEventListener('scroll', _scrollHandler)
+  document.addEventListener('wheel', _defaultHandler)
+  document.addEventListener('DOMMouseScroll', _defaultHandler)
+  document.addEventListener('touchmove', _defaultHandler)
+  document.addEventListener('keydown', _keydownHandler)
 }
 
 export function scrollBlockOff () {
-  document.removeEventListener('scroll', scrollHandler)
-  document.removeEventListener('wheel', defaultHandler)
-  document.removeEventListener('DOMMouseScroll', defaultHandler)
-  document.removeEventListener('touchmove', defaultHandler)
-  document.removeEventListener('keydown', keydownHandler)
+  document.removeEventListener('scroll', _scrollHandler)
+  document.removeEventListener('wheel', _defaultHandler)
+  document.removeEventListener('DOMMouseScroll', _defaultHandler)
+  document.removeEventListener('touchmove', _defaultHandler)
+  document.removeEventListener('keydown', _keydownHandler)
 }
 
-function defaultHandler (event) {
+function _defaultHandler (event) {
   event.preventDefault()
 }
 
-function keydownHandler (event) {
+function _keydownHandler (event) {
   if (scrollKeys.includes(event.keyCode)) {
     event.preventDefault()
   }
 }
 
-function scrollHandler (event) {
+function _scrollHandler (event) {
   event.preventDefault()
   window.scrollTo(pageXOffset, pageYOffset)
 }
