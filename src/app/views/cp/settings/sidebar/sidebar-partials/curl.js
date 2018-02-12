ae0f3643bd67ec7f2a008dbefb8321ee3d92aecb
gariasf <gariasfa7@alumnes.ub.edu>
2018-02-12 17:30:32 +0100

    Add cURL command copy button

--- src/app/views/cp/settings/sidebar/sidebar-partials/curl.js
+++ src/app/views/cp/settings/sidebar/sidebar-partials/curl.js
@@ -1,6 +1,7 @@
 import {h, Component} from 'preact'
 import {Link} from 'react-router-dom'
 import {connect} from 'preact-redux'
+import Ink from 'react-ink'
 
 import Button from '../../../../shared/button'
 import {copyToClipboard} from 'utils'
@@ -36,31 +37,37 @@ export default connect(mapStateToProps)(class CurlPartial extends Component {
   }
 
   render ({userData}) {
+    const curlCommand = `curl -X POST \\
+                  ${apiBaseUrl.replace('graphql', 'api')} \\
+                  -H 'api-key: ${userData.apiKey}' \\
+                  -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \\
+                  -F 'file=@/absolute/path/to/file'`
     return (
       <div class={style.section}>
         <div class='flex flex-cross-center'><Link to='/cp/settings'><Button customClass={style.backButton} iconButton icon='back' /></Link>&nbsp;<h3>Using cURL</h3></div>
         <div class={style.sectionInfo}>
           <p>
             You can upload files using cURL in your terminal on Mac and Linux.<br />
+            Use the command provided below replacing <code>/absolute/path/to/file</code> with the path of the file you want to upload.
           </p>
 
           <div class={style.sectionInfo}>
             <div class={style.terminalWindow}>
-              <header>
-                <div class={`${style.terminalWindowButton} ${style.red}`} />
-                <div class={`${style.terminalWindowButton} ${style.yellow}`} />
-                <div class={`${style.terminalWindowButton} ${style.green}`} />
+              <header class='flex flex-cross-center flex-sb'>
+                <div>
+                  <div class={`${style.terminalWindowButton} ${style.red}`} />
+                  <div class={`${style.terminalWindowButton} ${style.yellow}`} />
+                  <div class={`${style.terminalWindowButton} ${style.green}`} />
+                </div>
+                <button data-copytext={curlCommand} onClick={this.handleCopyClick} class='flex flex-full-center'>
+                  <Ink />{this.state.valueCopied ? 'Copied!' : 'Copy'}</button>
               </header>
               <section>
                 <span class={style.terminalBlue}>{userData.name}</span>
                 @<span class={style.terminalLightBlue}>nethloader_pc</span>&nbsp;
                 <span class={style.terminalBlue}>$</span>&nbsp;
                 {
-                  `curl -X POST \\
-                  ${apiBaseUrl.replace('graphql', 'api')} \\
-                  -H 'api-key: ${userData.apiKey}' \\
-                  -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \\
-                  -F 'file=@/absolute/path/to/file'`.split('\n').map((line, key) => <span key={key}>{line}<br /></span>) // If you see this, don't()
+                  curlCommand.split('\n').map((line, key) => <span key={key}>{line}<br /></span>) // If you see this, don't()
                 }
                 <span class={style.cursor} />
               </section>
