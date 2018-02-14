cefdd3422169d3599d95a2e9e8deb97a869b63ba
gariasf <gariasfa7@alumnes.ub.edu>
2018-02-14 20:34:17 +0100

    Add meta tags to media view

--- src/app/views/media-view/index.js
+++ src/app/views/media-view/index.js
@@ -19,14 +19,67 @@ function mapStateToProps (state) {
 }
 
 export default connect(mapStateToProps)(class MediaView extends Component {
+  constructor (props) {
+    super(props)
+
+    this.metas = {}
+    this.computeMeta = this.renderMeta.bind(this)
+  }
+
   componentWillMount () {
     window.scrollTo(0, 0) // Quick hack to fix react-routing scroll issue
     this.props.dispatch(getMediaInfo(this.context.router.route.match.params.id)).then((data) => {
       this.mediaSrc = `${baseMediaPath}${data.id}.${data.extension}`
       this.mediaUrl = `${document.location.origin}/${data.id}`
+    }).then(() => {
+      this.renderMeta()
     }).catch(() => {
       this.context.router.history.push('/404')
     })
+  }
+
+  componentWillUnmount () {
+    Object.values(this.metas).forEach(element => element.remove())
+  }
+
+  renderMeta () {
+    this.metas.twitterTitle = document.createElement('meta')
+    this.metas.twitterTitle.name = 'twitter:title'
+    this.metas.twitterTitle.content = 'Nethloader: Media sharing service'
+
+    this.metas.twitterSite = document.createElement('meta')
+    this.metas.twitterSite.name = 'twitter:site'
+    this.metas.twitterSite.content = '@Nethloader'
+
+    this.metas.twitterDesc = document.createElement('meta')
+    this.metas.twitterDesc.name = 'twitter:description'
+    this.metas.twitterDesc.content = 'Nethloader, a self hosted media sharing service'
+
+    this.metas.twitterImg = document.createElement('meta')
+    this.metas.twitterImg.name = 'twitter:image'
+    this.metas.twitterImg.content = this.mediaSrc
+
+    this.metas.ogSiteName = document.createElement('meta')
+    this.metas.ogSiteName.name = 'og:site_name'
+    this.metas.ogSiteName.content = 'Nethloader'
+
+    this.metas.ogTitle = document.createElement('meta')
+    this.metas.ogTitle.name = 'og:title'
+    this.metas.ogTitle.content = 'Nethloader: Media sharing service'
+    
+    this.metas.ogImage = document.createElement('meta')
+    this.metas.ogImage.name = 'og:image'
+    this.metas.ogImage.content = this.mediaSrc
+
+    this.metas.ogDesc = document.createElement('meta')
+    this.metas.ogDesc.name = 'og:description'
+    this.metas.ogDesc.content = 'Nethloader, a self hosted media sharing service'
+
+    this.metas.ogType = document.createElement('meta')
+    this.metas.ogType.name = 'og:type'
+    this.metas.ogType.content = 'website'
+
+    Object.values(this.metas).forEach(element => { document.head.appendChild(element) })
   }
 
   render ({mediaInfo, isFetching}) {
