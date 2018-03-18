efab4730e53960506dc5874f5623661c1e567f33
gariasf <gariasfa7@alumnes.ub.edu>
2018-03-19 00:52:29 +0100

    0.2.1

--- src/assets/app.config.js
+++ src/assets/app.config.js
@@ -1,11 +1,8 @@
 
-/* eslint-disable no-unused-vars */
-const version = '0.2.0'
+const version = '0.2.1' // Do not edit this value
 
 const apiBaseUrl = 'http://localhost:4000/graphql'
 
 const baseMediaPath = 'http://localhost:4000/media/' // IMPORTANT: Has to preserve the last trailing slash
 
 const adminEmail = 'supportadmin@domain.com'
-
-/* eslint-enable no-unused-vars */
