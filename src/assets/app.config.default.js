d4ec70369615916d15c5d84ab19cacc06fc24a1c
gariasf <gariasfa7@alumnes.ub.edu>
2018-03-19 16:41:42 +0100

    Add default (backup) config file
    Fix html scripts mime type

---
+++ src/assets/app.config.default.js
@@ -0,0 +1,5 @@
+const apiBaseUrl = 'http://localhost:4000/graphql'
+
+const baseMediaPath = 'http://localhost:4000/media/' // IMPORTANT: Has to preserve the last trailing slash
+
+const adminEmail = 'supportadmin@domain.com'
