dc4588c34b0ae3b9c74637e2772bfa951081b93b
gariasf <gariasfa7@alumnes.ub.edu>
2018-03-19 01:26:44 +0100

    Remove version from config

--- src/app/views/shared/footer/index.js
+++ src/app/views/shared/footer/index.js
@@ -5,6 +5,8 @@ import Icon from '../icon'
 
 import style from './styles.scss'
 
+import version from 'version'
+
 const viewStrings = locale.footer // eslint-disable-line no-undef
 
 export default function Footer ({contentFooter}) {
@@ -12,7 +14,7 @@ export default function Footer ({contentFooter}) {
     <footer class={`${style.footer} ${contentFooter ? style.footerContent : ''}`}>
       <span class='flex flex-full-center'>
         <a class='flex flex-full-center' href='https://github.com/nethruster/nethloader' target='_blank' rel='noopener'>
-          <Icon iconName='github' />&nbsp;<p>{viewStrings.powered_by}&nbsp;Nethloader v{version}</p> {/* eslint-disable-line no-undef */}
+          <Icon iconName='github' />&nbsp;<p>{viewStrings.powered_by}&nbsp;Nethloader v{version}</p>
         </a>&nbsp;&nbsp;&nbsp;&nbsp;
         <a class={`flex flex-full-center ${style.twitter}`} href='https://twitter.com/nethruster' rel='noopener' target='_blank'>
           <Icon iconName='twitter' />&nbsp;<p>nethruster</p>
--- src/assets/app.config.js
+++ src/assets/app.config.js
@@ -1,6 +1,3 @@
-
-const version = '0.2.1' // Do not edit this value
-
 const apiBaseUrl = 'http://localhost:4000/graphql'
 
 const baseMediaPath = 'http://localhost:4000/media/' // IMPORTANT: Has to preserve the last trailing slash
