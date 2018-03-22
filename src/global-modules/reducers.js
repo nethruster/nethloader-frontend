f93370cacc55451fdcf6632bc811e8c31115db5d
Claudio4 <me@claudio4.com>
2018-03-22 00:11:26 +0000

    Bug fix

--- src/global-modules/reducers.js
+++ src/global-modules/reducers.js
@@ -6,7 +6,7 @@ import {userData, userMedia} from 'reducers/data'
 import {mediaUpload, mediaInfo, mediaDelete, mediaSelect} from 'reducers/media'
 import html5video from 'reducers/html5video'
 import settings from 'reducers/settings'
-import {users, userSelect, toggleIsAdmin, adminMediaSelect} from 'reducers/admin-settings'
+import {users, userSelect, toggleIsAdmin} from 'reducers/admin-settings'
 
 const reducers = combineReducers({
   authentication,
@@ -21,7 +21,6 @@ const reducers = combineReducers({
   users,
   userSelect,
   toggleIsAdmin,
-  adminMediaSelect,
   snackbar: snackbarReducer
 })
 
